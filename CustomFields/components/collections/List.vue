ceee2cfa7d614c0d363fda0c0c85ded0d4661b02
500apps-sivamani <sivamani.munaga@500apps.com>
2023-04-09 18:05:35 +0530

    Custom Fields

---
+++ CustomFields/components/collections/List.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="bg-gray-900 py-10">
+    <div class="px-4 sm:px-6 lg:px-8">
+      <div class="sm:flex sm:items-center">
+        <div class="sm:flex-auto">
+          <h1 class="text-base font-semibold leading-6 text-white">
+            Add Your Custom Fields
+          </h1>
+          <p class="mt-2 text-sm text-gray-300">
+            Please Enter your custom field By click on "Add custom field"
+          </p>
+        </div>
+        <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+          <button
+            @click="addCustomfield"
+            type="button"
+            class="block rounded-md bg-indigo-500 px-3 py-2 text-center text-sm font-semibold text-white hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+          >
+            Add custom field
+          </button>
+        </div>
+      </div>
+      <div class="mt-8 flow-root">
+        <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div
+            class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8"
+          >
+            <table class="min-w-full divide-y divide-gray-700">
+              <thead>
+                <tr>
+                  <th
+                    scope="col"
+                    class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-white sm:pl-0"
+                  >
+                    Name
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-3 py-3.5 text-left text-sm font-semibold text-white"
+                  >
+                    Type
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-3 py-3.5 text-left text-sm font-semibold text-white"
+                  >
+                    Description
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-3 py-3.5 text-left text-sm font-semibold text-white"
+                  >
+                    Placeholder
+                  </th>
+                  <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                    <span class="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody class="divide-y divide-gray-800">
+                <tr
+                  v-for="(customFieldData, index) in getCustomData"
+                  :key="index"
+                >
+                  <td
+                    class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0"
+                  >
+                    {{ customFieldData.name }}
+                  </td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-300">
+                    {{ customFieldData.type }}
+                  </td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-300">
+                    {{ customFieldData.type_data.description }}
+                  </td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-300">
+                    {{ customFieldData.type_data.placeholder }}
+                  </td>
+                  <td
+                    class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0"
+                  >
+                    <a
+                      href="#"
+                      class="text-indigo-400 hover:text-indigo-300"
+                      @click="editCall(customFieldData, index)"
+                      >Edit</a
+                    >
+                  </td>
+                  <td
+                    class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0"
+                  >
+                    <a
+                      href="#"
+                      class="text-indigo-400 hover:text-indigo-300"
+                      @click="deleteCall(customFieldData, index)"
+                      >Delete</a
+                    >
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineEmits, defineProps } from "vue";
+
+const emit = defineEmits(["openCustomfieldSidebar", "editCall", "deleteCall"]);
+
+const props = defineProps({
+  getCustomData: {
+    type: Array,
+  },
+});
+
+//Sending emit for opening sidebar
+const addCustomfield = () => {
+  emit("openCustomfieldSidebar");
+};
+
+//sending emit for edit
+const editCall = (customFieldData: any, index: any) => {
+  emit("editCall", customFieldData, index);
+};
+
+//sending emit for delete
+const deleteCall = (customFieldData: any, index: any) => {
+  emit("deleteCall", customFieldData, index);
+};
+</script>
